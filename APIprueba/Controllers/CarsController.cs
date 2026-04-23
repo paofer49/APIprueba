03f96f551b5da05089ea46659d7453827ef3c607
PAOLA FERNANDA RUBI BACA <112739@alumnouninter.mx>
2026-04-22 18:38:38 -0600

    -

--- APIprueba/Controllers/CarsController.cs
+++ APIprueba/Controllers/CarsController.cs
@@ -1,7 +1,8 @@
 ﻿using APIprueba.Data;
+using APIprueba.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using APIprueba.Models;
+using System.Runtime.ConstrainedExecution;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -36,34 +37,58 @@ namespace APIprueba.Controllers
 
         // POST api/<CarsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] Cars car)
         {
+            _db.Cars.Add(car);
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new {Id = car.Id},
+                new
+                {
+                    OK = true,
+                    mensaje = "Registro creado exitosamente",
+                    data = car
+                });
         }
 
         // PUT api/<CarsController>/5
         [HttpPut("{id}")]
         public async Task<IActionResult> Put(int id, [FromBody] Cars cars)
         {
-            if (id != cars.Id)
-                return BadRequest(new { exito = false, mensaje = "El ID no coincide" });
+            var consulta = await _db.Cars.FindAsync(id);
+            consulta.Id = cars.Id;
+            consulta.Name = cars.Name;
+            consulta.Color = cars.Color;
 
-            var existing = await _db.Cars.FindAsync(id);
-
-            if (existing == null)
-                return NotFound(new { exito = false, mensaje = "El ID no fue encontrado" });
-
-            existing.Name = cars.Name;
-            existing.Color = cars.Color;
-
+            _db.Entry(consulta).State = EntityState.Modified;
             await _db.SaveChangesAsync();
 
-            return Ok(new { exito = true, existing });
+            return Ok(
+                new
+                {
+                    OK = true,
+                    mensaje = "Registro creado exitosamente",
+                    data = cars
+                });
         }
 
         // DELETE api/<CarsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            var consulta = await _db.Cars.FindAsync(id);
+
+            _db.Cars.Remove(consulta);
+            await _db.SaveChangesAsync();
+
+            return Ok(
+                new
+                {
+                    OK = true,
+                    mensaje = "Registro creado exitosamente",
+                    data = consulta
+                });
+
         }
     }
 }
--- APIprueba/Controllers/Tabla1Controller.cs
+++ APIprueba/Controllers/Tabla1Controller.cs
@@ -1,6 +1,8 @@
 ﻿using APIprueba.Data;
+using APIprueba.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -35,20 +37,56 @@ namespace APIprueba.Controllers
 
         // POST api/<Tabla1Controller>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] Tabla1 registro)
         {
+            _db.Tabla1.Add(registro);
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { Id = registro.Id },
+                new
+                {
+                    OK = true,
+                    mensaje = "Registro creado exitosamente",
+                    data = registro
+                });
         }
 
         // PUT api/<Tabla1Controller>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(int id, [FromBody] Tabla1 registro)
         {
+            var consulta = await _db.Tabla1.FindAsync(id);
+            consulta.Id = registro.Id;
+            consulta.Nombre = registro.Nombre;
+
+            _db.Entry(consulta).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+
+            return Ok(
+                new
+                {
+                    OK = true,
+                    mensaje = "Registro creado exitosamente",
+                    data = registro
+                });
         }
 
         // DELETE api/<Tabla1Controller>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            var consulta = await _db.Tabla1.FindAsync(id);
+            _db.Tabla1.Remove(consulta);
+
+            await _db.SaveChangesAsync();
+
+            return Ok(
+                new
+                {
+                    OK = true,
+                    mensaje = "Registro creado exitosamente",
+                    data = consulta
+                });
         }
     }
 }
